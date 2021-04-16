78ca2c9704b076b9d0949d60bda2d7b8146eeb65
Yuqi Huai <yhuai@uci.edu>
2021-04-16 11:10:55 +0800

    Create utils

---
+++ utils/__init__.py
@@ -0,0 +1,46 @@
+import logging
+import re
+
+
+def get_logger(name, filename=None):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
+    # LOGGER FILE HANDLER
+    # if not os.path.exists("Logs"):
+    #     os.mkdir("Logs")
+    # fh = logging.FileHandler(f"Logs/{filename if filename else name}.log")
+    # fh.setLevel(logging.DEBUG)
+    # fh.setFormatter(formatter)
+    # logger.addHandler(fh)
+
+    # LOGGER STREAM HANDLER
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+
+    return logger
+
+
+def parse_course_id(url: str) -> str:
+    result = re.search(r"\/courses\/(\d+)\/?", url)
+    if result:
+        return result.group(1)
+    return ''
+
+
+def parse_discussion_id(url: str) -> str:
+    result = re.search(r"\/discussion_topics\/(\d+)\/?", url)
+    if result:
+        return result.group(1)
+    return ''
+
+
+def parse_assignment_id(url: str) -> str:
+    result = re.search(r"\/assignments\/(\d+)\/?")
+    if result:
+        return result.group(1)
+    return ''
---
+++ utils/config.py
@@ -0,0 +1,3 @@
+class Config:
+    def __init__(self, config: dict):
+        self.auth_token = config['IDENTIFICATION']['AUTH_TOKEN']
